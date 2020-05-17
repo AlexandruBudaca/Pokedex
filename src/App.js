1e182404368c8fb15720da0287b61e32cc39b72c
Alex <alex.andrul10@gmail.com>
2020-05-17 13:43:39 +0100

    new component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import BestPokemon from "./BestPokemon";
 
-function App() {
+function Logo() {
   return (
     <div>
       <header>
         <h1>Welcome to the Pokedex</h1>
         <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"></img>
       </header>
+    </div>
+  );
+}
+function App() {
+  return (
+    <div>
+      <Logo />
+      <BestPokemon />
     </div>
   );
 }
---
+++ src/BestPokemon.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <p>My favourite Pokemon is Squirtle"</p>;
+}
+
+export default App;
