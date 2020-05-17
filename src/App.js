3d181234edb83bf6c2f78e3f49c4735db44d7e63
Alex <alex.andrul10@gmail.com>
2020-05-17 13:28:14 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to the Pokedex</h1>
+        <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"></img>
+      </header>
+    </div>
+  );
+}
+export default App;
