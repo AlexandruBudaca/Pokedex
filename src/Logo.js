39d7ef833bea288394fb243580d9d2934d168337
Alex <alex.andrul10@gmail.com>
2020-06-07 14:06:07 +0100

    dependency

--- src/Logo.js
+++ src/Logo.js
@@ -4,13 +4,15 @@ const Logo = (props) => {
   // console.log(props);
 
   return (
-    <header className="container">
+    <header>
       <h1>Welcome to the {props.appName}</h1>
-      <img
-        onClick={props.clickAction}
-        src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
-        alt=""
-      />
+      <div className="container">
+        <img
+          onClick={props.clickAction}
+          src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
+          alt=""
+        />
+      </div>
     </header>
   );
 };
